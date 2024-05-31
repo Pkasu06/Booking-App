2d97254e295e00ba6fc3bdcebd247077e26c975c
jesusdoza <jesus.doza@gmail.com>
2024-05-31 08:05:18 -0500

    admin layout now server component

--- client/app/admin/layout.jsx
+++ client/app/admin/layout.jsx
@@ -1,19 +1,6 @@
-"use client";
-import { usePathname } from "next/navigation";
-import { auth } from "../firebase/config";
-import { useRouter } from "next/navigation";
-import { useAuthState } from "react-firebase-hooks/auth";
 import AdminMenu from "../_components/Admin/AdminMenu";
 
 export default function Layout({ children }) {
-  const pathname = usePathname();
-  const [user, loading, error] = useAuthState(auth);
-  const router = useRouter();
-
-  if (!user) {
-    router.push("/adminSignInAndSignUp");
-  }
-
   return (
     <div className="drawer bg-white">
       <input
