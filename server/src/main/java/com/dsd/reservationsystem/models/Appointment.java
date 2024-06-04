81f288985d17d153fd0fa075c0e1d7a1b4b9bf5f
jesusdoza <jesus.doza@gmail.com>
2024-06-04 08:44:44 -0500

    add timestamp to Appointment class

--- server/src/main/java/com/dsd/reservationsystem/models/Appointment.java
+++ server/src/main/java/com/dsd/reservationsystem/models/Appointment.java
@@ -1,78 +1,88 @@
 package com.dsd.reservationsystem.models;
 
 
+import com.google.cloud.Timestamp;
+
 //used inside customer db in appointmentsArray
 public class Appointment {
-    private String customerId;
-    private String date;
-    private String timeSlot;
-    private String confirmationNumber;
-    private String serviceId;
-    private String Status = "PENDING";
+  private String customerId;
+  private String date;
+  private String timeSlot;
+  private String confirmationNumber;
+  private String serviceId;
+  private String Status = "PENDING";
+  private Timestamp timestamp;
 
-    public Appointment() {
-    }
-    public Appointment(AppointmentPostRequest appointmentPostRequest) {
-    }
+  public Appointment() {
+  }
 
-    public String getTimeSlot() {
-        return timeSlot;
-    }
+  public Timestamp getTimestamp() {
+    return timestamp;
+  }
 
-    public void setTimeSlot(String timeSlot) {
-        this.timeSlot = timeSlot;
-    }
+  public void setTimestamp(Timestamp timestamp) {
+    this.timestamp = timestamp;
+  }
 
-    public String getConfirmationNumber() {
-        return confirmationNumber;
-    }
+  public String getTimeSlot() {
+    return timeSlot;
+  }
 
-    public void setConfirmationNumber(String confirmationNumber) {
-        this.confirmationNumber = confirmationNumber;
-    }
+  public void setTimeSlot(String timeSlot) {
+    this.timeSlot = timeSlot;
+  }
+
+  public String getConfirmationNumber() {
+    return confirmationNumber;
+  }
+
+  public void setConfirmationNumber(String confirmationNumber) {
+    this.confirmationNumber = confirmationNumber;
+  }
 
 
-    public String getDate() {
-        return date;
-    }
+  public String getDate() {
+    return date;
+  }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
+  public void setDate(String date) {
+    this.date = date;
+  }
 
-    public String getServiceId() {
-        return serviceId;
-    }
+  public String getServiceId() {
+    return serviceId;
+  }
 
-    public void setServiceId(String serviceId) {
-        this.serviceId = serviceId;
-    }
+  public void setServiceId(String serviceId) {
+    this.serviceId = serviceId;
+  }
 
-    public String getStatus() {
-        return Status;
-    }
+  public String getStatus() {
+    return Status;
+  }
 
-    public void setStatus(String status) {
-        Status = status;
-    }
+  public void setStatus(String status) {
+    Status = status;
+  }
 
-    public String getCustomerId() {
-        return customerId;
-    }
+  public String getCustomerId() {
+    return customerId;
+  }
 
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
+  public void setCustomerId(String customerId) {
+    this.customerId = customerId;
+  }
 
-    @Override
-    public String toString() {
-        return "Appointment{" +
-                "customerId='" + customerId + '\'' +
-                ", date='" + date + '\'' +
-                ", timeSlot='" + timeSlot + '\'' +
-                ", confirmationNumber='" + confirmationNumber + '\'' +
-                ", serviceId='" + serviceId + '\'' +
-                ", Status='" + Status + '\'' +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Appointment{" +
+        "customerId='" + customerId + '\'' +
+        ", date='" + date + '\'' +
+        ", timeSlot='" + timeSlot + '\'' +
+        ", confirmationNumber='" + confirmationNumber + '\'' +
+        ", serviceId='" + serviceId + '\'' +
+        ", Status='" + Status + '\'' +
+        ", timestamp=" + timestamp +
+        '}';
+  }
 }
