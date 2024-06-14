55bed209e2b4f19a2f9815c5a1f3e2e71571a263
jesusdoza <jesus.doza@gmail.com>
2024-06-14 07:39:26 -0500

    auth credentials provider scaffold

--- client/auth.ts
+++ client/auth.ts
@@ -1,5 +1,45 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { auth as firebaseAuth } from "./app/firebase/config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        let user = null;
+        const { email, password } = credentials;
+
+        // logic to salt and hash password
+        const pwHash = credentials.password;
+
+        // logic to verify if user exists
+        try {
+          const userCredential = await signInWithEmailAndPassword(
+            firebaseAuth,
+            email as string,
+            password as string
+          );
+          const token = await userCredential.user.getIdToken();
+          return { id: userCredential.user.uid, user: userCredential };
+        } catch (error) {
+          console.log("Error during sign in:", error.message);
+          return null;
+        }
+
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // meaning this is also the place you could do registration
+          throw new Error("User not found.");
+        }
+
+        // return user object with the their profile data
+        return user;
+      },
+    }),
+  ],
 });
